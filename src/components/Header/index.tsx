cea9fa15562ec325383da62bf4b7c7ae51cd7b8b
Pranav Sharma <mechanicm56@gmail.com>
2025-08-28 12:29:04 +0530

    minor fixes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,12 @@
 import styles from './style.module.css'
-import logo from '../../assets/rocket.svg'
+import logo from '../../assets/ai.svg'
 
 export function Header() {
   return (
     <header className={styles.header}>
       <img src={logo} alt="todo list logo" />
       <h1>
-        <span>to</span>
+        <span>To</span>
         <span>do</span>
       </h1>
     </header>
